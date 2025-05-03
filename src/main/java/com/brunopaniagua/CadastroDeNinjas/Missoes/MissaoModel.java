50cb2fdcfac10088d722d33534562ac726612c87
Bruno Paniagua <brunopaniaguaoliveira@gmail.com>
2025-05-03 14:15:51 -0300

    feat: Coloca o Lombok nas classes

--- src/main/java/com/brunopaniagua/CadastroDeNinjas/Missoes/MissaoModel.java
+++ src/main/java/com/brunopaniagua/CadastroDeNinjas/Missoes/MissaoModel.java
@@ -2,11 +2,17 @@ package com.brunopaniagua.CadastroDeNinjas.Missoes;
 
 import com.brunopaniagua.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
-@Table(name = "tb_missoes")
+@Table(name = "tb_missao")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissaoModel {
 
     @Id
@@ -20,46 +26,4 @@ public class MissaoModel {
     // Muitos ninjas fazem parte de uma missão
     @OneToMany(mappedBy = "missao")
     private List<NinjaModel> ninjas;
-
-    public MissaoModel() {
-    }
-
-    public MissaoModel(long id, String nome, String dificuldade, List<NinjaModel> ninjas) {
-        this.id = id;
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-        this.ninjas = ninjas;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public List<NinjaModel> getNinjas() {
-        return ninjas;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
-
-    public void setNinjas(List<NinjaModel> ninjas) {
-        this.ninjas = ninjas;
-    }
 }
