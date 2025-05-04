f7383ebb199973d62e8427dde7ff73a77bdb6e36
Bruno Paniagua <brunopaniaguaoliveira@gmail.com>
2025-05-04 16:45:29 -0300

    feat: Cria as rotas do mock do MissaoController

--- src/main/java/com/brunopaniagua/CadastroDeNinjas/Missoes/MissaoController.java
+++ src/main/java/com/brunopaniagua/CadastroDeNinjas/Missoes/MissaoController.java
@@ -1,4 +1,29 @@
 package com.brunopaniagua.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissaoController {
+
+    @GetMapping("/listar")
+    public String listarMissao(){
+        return "Lista de missoes";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missao criada com sucesso!";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao(){
+        return "Missao alterada com sucesso!";
+    }
+
+    @DeleteMapping
+    public String excluirMissao(){
+        return "Missao deletada com sucesso!";
+    }
+
 }
