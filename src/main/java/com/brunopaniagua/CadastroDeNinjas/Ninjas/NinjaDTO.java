134dbbdccc4753aa7419bd4f487d5dbd1657d2e1
Bruno Paniagua <brunopaniaguaoliveira@gmail.com>
2025-05-06 22:01:54 -0300

    chore: Inclui a versao do DTO pela entidade e mapeia manualmente no NinjaMapper

--- src/main/java/com/brunopaniagua/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/com/brunopaniagua/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,21 @@
 package com.brunopaniagua.CadastroDeNinjas.Ninjas;
 
+import com.brunopaniagua.CadastroDeNinjas.Missoes.MissaoModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String imgUrl;
+    private String rank;
+    private MissaoModel missao;
+
 }
