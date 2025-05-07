309bf1c06d5199d832737e1f0a5e484b3ff2ba50
Bruno Paniagua <brunopaniaguaoliveira@gmail.com>
2025-05-07 10:15:09 -0300

    feat: Criando DTO e Mapper para a entidade Missao

---
+++ src/main/java/com/brunopaniagua/CadastroDeNinjas/Missoes/MissaoMapper.java
@@ -0,0 +1,48 @@
+package com.brunopaniagua.CadastroDeNinjas.Missoes;
+
+import com.brunopaniagua.CadastroDeNinjas.Ninjas.NinjaDTO;
+import com.brunopaniagua.CadastroDeNinjas.Ninjas.NinjaMapper;
+import com.brunopaniagua.CadastroDeNinjas.Ninjas.NinjaModel;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class MissaoMapper {
+
+    NinjaMapper ninjaMapper;
+
+    public MissaoMapper(NinjaMapper ninjaMapper) {
+        this.ninjaMapper = ninjaMapper;
+    }
+
+    public MissaoModel map(MissaoDTO missaoDTO){
+        MissaoModel missaoModel = new MissaoModel();
+        missaoModel.setId(missaoDTO.getId());
+        missaoModel.setNome(missaoDTO.getNome());
+        missaoModel.setDificuldade(missaoDTO.getDificuldade());
+
+        List<NinjaModel> ninjas = missaoDTO.getNinjas().stream()
+                .map(ninjaMapper::map)
+                .collect(Collectors.toList());
+
+        missaoModel.setNinjas(ninjas);
+
+        return missaoModel;
+    }
+
+    public MissaoDTO map(MissaoModel missaoModel){
+        MissaoDTO missaoDTO = new MissaoDTO();
+        missaoDTO.setId(missaoModel.getId());
+        missaoDTO.setNome(missaoModel.getNome());
+        missaoDTO.setDificuldade(missaoModel.getDificuldade());
+
+        List<NinjaDTO> ninjas = missaoModel.getNinjas().stream()
+                .map(ninjaMapper::map)
+                .collect(Collectors.toList());
+        missaoDTO.setNinjas(ninjas);
+
+        return missaoDTO;
+    }
+}
